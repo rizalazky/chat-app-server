5c645537b0b1e24f57f90dd1c7179fbcaa188f89
rizalazky <rizalazky26@gmail.com>
2022-06-18 10:22:59 +0700

    chat app server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3200
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
